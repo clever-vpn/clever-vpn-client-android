c9324a0b87ed1952306ba6a86071f1a044dc0b79
bolinwu <bolinwu@hotmail.com>
2026-05-26 11:45:53 +0800

    chore: align workflow and externalize kit version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 
 val pkg: String = providers.gradleProperty("clevervpnPackageName").get()
 val ns: String = providers.gradleProperty("clevervpnSpacename").get()
+val cleverVpnAndroidKitVersion: String = providers.gradleProperty("clevervpnAndroidKitVersion").get()
 
 plugins {
     alias(libs.plugins.android.application)
@@ -77,7 +78,7 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 //    implementation(project(":kit"))
-    implementation("net.clever-vpn:clever-vpn-android-kit:1.0.20250821")
+    implementation("net.clever-vpn:clever-vpn-android-kit:$cleverVpnAndroidKitVersion")
     coreLibraryDesugaring(libs.desugarJdkLibs)
     implementation(libs.camera.core)
     implementation(libs.camera.lifecycle)
